573f51b06aea1ec022dc1eda3712061f01727914
Harsh Sharma <haresh2pandit@gmail.com>
2026-04-03 21:21:07 +0530

    Switching to HashRouter for foolproof routing and removing vercel.json

--- helix-frontend/src/App.jsx
+++ helix-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AnimatePresence } from 'framer-motion';
 
